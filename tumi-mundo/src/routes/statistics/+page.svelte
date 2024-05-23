39e9e1d5324e346bb2083288faf8beaad1aff330
senanurkorkutan <snnrkrk1@gmail.com>
2024-05-23 13:07:45 +0200

    statistics page back-up

--- tumi-mundo/src/routes/statistics/+page.svelte
+++ tumi-mundo/src/routes/statistics/+page.svelte
@@ -1,6 +1,3 @@
-<script>
-
-</script>
 
 <svelte:head>
 	<link rel="stylesheet" href="../lib/styles/style.css" />
@@ -27,35 +24,67 @@
         <h2>Total activities</h2>
         <img src="/" alt="Raymond is sitting on the couch">
 
-        <section>
-            <h3>You have done a lot of learning!</h3>
+        <div>
+            <h2>You have done a lot of learning!</h2>
             <p>Here you can see everything you have done since you started learning with TuMi Mundo. 
     I encourage you to keep going and do your best!</p>
-        </section>
+        </div>
 
-        <section>
-            <h3>The attention span test</h3>
-            <p>A test for the first phase of the method. Tap on the points to see more in depth numbers.</p>
+    </section>
+    
+    <section>
+        <h2>The attention span test</h2>
+        <p>A test for the first phase of the method. Tap on the points to see more in depth numbers.</p>
 
-            <!-- graphic -->
+        <!-- graphic -->
 
-            <label>
-                See how other children are performing
-                <input type="checkbox" name="checkbox" value="value">
-            </label>
-        </section>
+        <label>
+            See how other children are performing
+            <input type="checkbox" name="checkbox" value="value">
+        </label>
+    </section>
 
-        <section>
-            <h3>Stories</h3>
-            <ul>
-                <li>
-                    
-                    <p>Stories read</p>
-                </li>
-            </ul>
-        </section>
-
+    <section>
+        <h2>Stories</h2>
+        
+        <ul class="stories_container">
+            <li>
+                <div class="">12</div>
+                <p>stories read</p>
+            </li>
+            <li>
+                <div class="">136</div>
+                <p>minutes spend listening</p>
+            </li>
+            <li>
+                <div class="">8</div>
+                <p>tests taken</p>
+            </li>
+            <li>
+                <div class="">Morning</div>
+                <p>is your favorite moment</p>
+            </li>
+            <li>
+                <div class="">Thuan-Hoa</div>
+                <p>is your favoirite speaker</p>
+            </li>
+        </ul>
     </section>
 
 
+    
 
+
+
+<style>
+    .stories_container {
+        display: flex;
+        flex-direction: column;      
+        
+        &li {
+            display: flex;
+            background: red;
+        }
+    }
+
+</style>
