357adc854d0d0d1d87e3fb1f3764a1f8df60833c
Brianne <92082302+briannededeugd@users.noreply.github.com>
2024-05-29 13:24:02 +0200

    FEAT: Redirect to statistics page after the test is completed

---
+++ tumi-mundo/src/routes/offboarding/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		function redirection() {
+			window.location.pathname = '/statistics';
+		}
+
+		setInterval(redirection(), 7500);
+	});
+</script>
+
+<div class="popup">
+	<div>
+		<p>Test completed! Please wait while we process your results.</p>
+	</div>
+</div>
